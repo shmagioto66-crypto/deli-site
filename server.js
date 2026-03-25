237228ee3473a7ff54c5df8b0eb0cf89f12d6d08
shmagi <shmagioto66@gmail.com>
2026-03-25 13:57:02 +0400

    fix server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// static files (html, css)
+// static files
 app.use(express.static('public'));
+
+// მთავარი გვერდი
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
