3dd0b2228d50bd23f9f05d91e043366ed79779e7
shmagi <shmagioto66@gmail.com>
2026-03-25 13:54:13 +0400

    fix

--- server.js
+++ server.js
@@ -10,3 +10,9 @@ app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
 
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
